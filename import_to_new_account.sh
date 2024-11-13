456881ef3374f08ca9da5f049a20b3ffce943d5e
casalino <casalino@adobe.com>
2024-11-13 13:46:35 -0500

    Fix MacOS commands and reorder exports to make sense

--- import_to_new_account.sh
+++ import_to_new_account.sh
@@ -8,19 +8,21 @@ export CLOUDFLARE_API_TOKEN='<your_user_API_token>'
 unset CLOUDFLARE_ACCOUNT_ID
 unset CLOUDFLARE_ZONE_ID
 export CLOUDFLARE_ACCOUNT_ID='<your_new_cloudflare_account_id>'
-export CLOUDFLARE_OLD_ACCOUNT_ID='<your_old_cloudflare_account_id>'
 export CLOUDFLARE_ZONE_ID='<your_new_cloudflare_zone_id>'
+export CLOUDFLARE_OLD_ACCOUNT_ID='<your_old_cloudflare_account_id>'
 export CLOUDFLARE_OLD_ZONE_ID='<your_old_cloudflare_zone_id>'
 export CLOUDFLARE_EMAIL='<your_account_email_id>'
 
 
-
-# Because the original generated file contains resources with the old zone ID, we need to replace it with the new zone ID.
+# Because the original generated file contains resources with the old zone and account IDs, we need to replace them with the new zone/account IDs.
 # You can note this step out if you aren't migrating the configuration to a new account.
 
 # If using homebrew on MacOS, you need to install gnu-sed for this command to work. Default sed on MacOS is not the same as on Linux.
 if [[ -f /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed ]]; then
     /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_config.tf
+    /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_zone_config.tf
+    /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ACCOUNT_ID/$CLOUDFLARE_ACCOUNT_ID/g" old_account_config.tf
+    /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ACCOUNT_ID/$CLOUDFLARE_ACCOUNT_ID/g" old_account_zone_config.tf
 else
     sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_config.tf
     sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_zone_config.tf
