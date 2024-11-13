8f878a8c10e802a2ec9c1547be5d69eabf9805b5
casalino <casalino@adobe.com>
2024-11-13 13:37:11 -0500

    Separate account vs zone resource types

--- generate_full_dump.sh
+++ generate_full_dump.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 # if using API Token
+unset CLOUDFLARE_API_TOKEN
 export CLOUDFLARE_API_TOKEN='<your_user_API_token>'
 
 # specify zone ID
+unset CLOUDFLARE_ZONE_ID
 export CLOUDFLARE_ZONE_ID='<your_old_cloudflare_zone_id>'
 
 # loop through every resource in the cf_generate_resource_types.txt and redirect the output to a new .tf file
 # If you get errors, your user API token may not have permissions to those resources
 # Check https://github.com/cloudflare/cf-terraforming for current supported resource types for generate
-while read resource || [[ -n $resource ]]; do cf-terraforming generate --resource-type $resource --zone $CLOUDFLARE_ZONE_ID; done < cf_generate_resource_types.txt> old_account_config.tf
+
+# All the configs which require zone
+while read resource || [[ -n $resource ]]; do cf-terraforming generate --resource-type $resource --zone $CLOUDFLARE_ZONE_ID; done < cf_generate_zone_required_resource_types.txt> old_account_zone_config.tf
+
+# All the configs which require just the account id
+unset CLOUDFLARE_ZONE_ID
+export CLOUDFLARE_ACCOUNT_ID='<your_old_cloudflare_account_id>'
+
+# Lists and such are account-specific, should limit these to a proper list of resources
+while read resource || [[ -n $resource ]]; do cf-terraforming generate --resource-type $resource; done < cf_generate_account_resource_types.txt> old_account_config.tf
+
--- import_to_new_account.sh
+++ import_to_new_account.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
 # if using API Token
+unset CLOUDFLARE_API_TOKEN
 export CLOUDFLARE_API_TOKEN='<your_user_API_token>'
 
-# specify zone ID
+# specify IDs
+unset CLOUDFLARE_ACCOUNT_ID
+unset CLOUDFLARE_ZONE_ID
+export CLOUDFLARE_ACCOUNT_ID='<your_new_cloudflare_account_id>'
+export CLOUDFLARE_OLD_ACCOUNT_ID='<your_old_cloudflare_account_id>'
 export CLOUDFLARE_ZONE_ID='<your_new_cloudflare_zone_id>'
+export CLOUDFLARE_OLD_ZONE_ID='<your_old_cloudflare_zone_id>'
 export CLOUDFLARE_EMAIL='<your_account_email_id>'
 
+
+
+# Because the original generated file contains resources with the old zone ID, we need to replace it with the new zone ID.
+# You can note this step out if you aren't migrating the configuration to a new account.
+
+# If using homebrew on MacOS, you need to install gnu-sed for this command to work. Default sed on MacOS is not the same as on Linux.
+if [[ -f /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed ]]; then
+    /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_config.tf
+else
+    sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_config.tf
+    sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_zone_config.tf
+    sed -i "s/$CLOUDFLARE_OLD_ACCOUNT_ID/$CLOUDFLARE_ACCOUNT_ID/g" old_account_config.tf
+    sed -i "s/$CLOUDFLARE_OLD_ACCOUNT_ID/$CLOUDFLARE_ACCOUNT_ID/g" old_account_zone_config.tf
+fi
+
 # Terraform 1.5+ only
-while read resource || [[ -n $resource ]]; do cf-terraforming import --resource-type $resource --modern-import-block --email $CLOUDFLARE_EMAIL --key $CLOUDFLARE_API_KEY --zone $CLOUDFLARE_ZONE_ID; done < cf_import_resource_types.txt 2>&1 | tee -a import_results.txt
+while read resource || [[ -n $resource ]]; do cf-terraforming import --resource-type $resource --modern-import-block --email $CLOUDFLARE_EMAIL --key $CLOUDFLARE_API_KEY --zone $CLOUDFLARE_ZONE_ID; done < cf_import_resource_types.txt >cloudflare_import_blocks.tfstate
+
+terraform plan -generate-config-out=generated.tf
+
+terraform apply
