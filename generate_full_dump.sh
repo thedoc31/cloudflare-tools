0126a8c04a2d11e1ee22701853df20c992335c32
casalino <casalino@adobe.com>
2024-11-18 15:03:32 -0500

    Update resource types and support multiple zones

--- generate_full_dump.sh
+++ generate_full_dump.sh
@@ -2,23 +2,44 @@
 
 # if using API Token
 unset CLOUDFLARE_API_TOKEN
-export CLOUDFLARE_API_TOKEN='<your_user_API_token>'
-
-# specify zone ID
-unset CLOUDFLARE_ZONE_ID
-export CLOUDFLARE_ZONE_ID='<your_old_cloudflare_zone_id>'
-
-# loop through every resource in the cf_generate_resource_types.txt and redirect the output to a new .tf file
-# If you get errors, your user API token may not have permissions to those resources
-# Check https://github.com/cloudflare/cf-terraforming for current supported resource types for generate
-
-# All the configs which require zone
-while read resource || [[ -n $resource ]]; do cf-terraforming generate --resource-type $resource --zone $CLOUDFLARE_ZONE_ID; done < cf_generate_zone_required_resource_types.txt> old_account_zone_config.tf
+export CLOUDFLARE_API_TOKEN='<your_api_token>'
+export CLOUDFLARE_EMAIL='you@wherever.com'
 
 # All the configs which require just the account id
-unset CLOUDFLARE_ZONE_ID
-export CLOUDFLARE_ACCOUNT_ID='<your_old_cloudflare_account_id>'
+
+unset CLOUDFLARE_ZONE_ID # force unset the zone ID because only zone or account can be specified at runtime
+export CLOUDFLARE_ACCOUNT_ID='<your_account_id>'
 
 # Lists and such are account-specific, should limit these to a proper list of resources
-while read resource || [[ -n $resource ]]; do cf-terraforming generate --resource-type $resource; done < cf_generate_account_resource_types.txt> old_account_config.tf
+if [[ -s "${CLOUDFLARE_ACCOUNT_ID}_account_config.tf" ]]; then
+    echo .
+    echo .
+    echo "  WARN:  ${CLOUDFLARE_ACCOUNT_ID}_account_config.tf already exists; skipping account export."
+else
+    echo "Generating ${CLOUDFLARE_ACCOUNT_ID}_account_config.tf..."
+    while read resource || [[ -n $resource ]]; do
+        cf-terraforming generate --resource-type "$resource" --account "$CLOUDFLARE_ACCOUNT_ID";
+    done < cf_generate_account_resource_scopes.txt> "${CLOUDFLARE_ACCOUNT_ID}_account_config.tf"
+fi
+
+# specify zone IDs
+unset CLOUDFLARE_ACCOUNT_ID # force unset the account ID because only zone or account can be specified at runtime
+CLOUDFLARE_ZONE_IDS=('<your_zone_id_1>' '<your_zone_id_2>' '<your_zone_id_n>')
+
+# Loop through each zone ID
+for CLOUDFLARE_ZONE_ID in "${CLOUDFLARE_ZONE_IDS[@]}"; do
+    # All the configs which require zone
+    if [[ -s "${CLOUDFLARE_ZONE_ID}_zone_config.tf" ]]; then
+        echo .
+        echo .
+        echo "  WARN:  ${CLOUDFLARE_ZONE_ID}_zone_config.tf already exists; it will be replaced."
+        rm -f ${CLOUDFLARE_ZONE_ID}_zone_config.tf
+    fi
+    echo .
+    echo .
+    echo "Generating ${CLOUDFLARE_ZONE_ID}_zone_config.tf..."
+    while read -r resource || [[ -n $resource ]]; do
+        cf-terraforming generate --resource-type "$resource" --zone "$CLOUDFLARE_ZONE_ID";
+    done < cf_generate_zone_required_resource_scopes.txt> "${CLOUDFLARE_ZONE_ID}_zone_config.tf"
+done
 
--- import_to_new_account.sh
+++ import_to_new_account.sh
@@ -2,37 +2,59 @@
 
 # if using API Token
 unset CLOUDFLARE_API_TOKEN
-export CLOUDFLARE_API_TOKEN='<your_user_API_token>'
+export CLOUDFLARE_API_TOKEN='<your_api_token>'
 
 # specify IDs
 unset CLOUDFLARE_ACCOUNT_ID
-unset CLOUDFLARE_ZONE_ID
-export CLOUDFLARE_ACCOUNT_ID='<your_new_cloudflare_account_id>'
-export CLOUDFLARE_ZONE_ID='<your_new_cloudflare_zone_id>'
-export CLOUDFLARE_OLD_ACCOUNT_ID='<your_old_cloudflare_account_id>'
-export CLOUDFLARE_OLD_ZONE_ID='<your_old_cloudflare_zone_id>'
-export CLOUDFLARE_EMAIL='<your_account_email_id>'
+unset CLOUDFLARE_ZONE_IDS
+export CLOUDFLARE_NEW_ACCOUNT_ID='<your_new_account_id>'
+export CLOUDFLARE_NEW_ZONE_IDS=('<your_zone_id_1>' '<your_zone_id_2>' '<your_zone_id_n>')
+export CLOUDFLARE_EMAIL='you@wherever.com'
 
+echo "Removing old tfstate files from current path..."
+rm -f *.tfstate
 
 # Because the original generated file contains resources with the old zone and account IDs, we need to replace them with the new zone/account IDs.
 # You can note this step out if you aren't migrating the configuration to a new account.
+for accountfile in $(ls -1 *_account_config.tf); do
+    if [[ -s $accountfile ]]; then
+        CLOUDFLARE_ACCOUNT_ID=$(echo "$accountfile" | cut -d'_' -f1)
+        echo "Attempting import of account $CLOUDFLARE_ACCOUNT_ID..."
+        echo "Transforming $CLOUDFLARE_ACCOUNT_ID to $CLOUDFLARE_NEW_ACCOUNT_ID..."
+        if [[ -f /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed ]]; then
+            /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_ACCOUNT_ID/$CLOUDFLARE_NEW_ACCOUNT_ID/g" "$accountfile"
+        else
+            sed -i "s/$CLOUDFLARE_ACCOUNT_ID/$CLOUDFLARE_NEW_ACCOUNT_ID/g" "$accountfile"
+        fi
+    else
+        echo "WARN: Account file is empty or does not exist."
+    fi
+    echo .
+    echo .
+    echo "Beginning import of resources for account $CLOUDFLARE_NEW_ACCOUNT_ID..."
+    while read resource || [[ -n "$resource" ]]; do cf-terraforming import --resource-type "$resource" --modern-import-block --email "$CLOUDFLARE_EMAIL" --key "$CLOUDFLARE_API_KEY" --token "$CLOUDFLARE_API_TOKEN" --account "$CLOUDFLARE_NEW_ACCOUNT_ID"; done < cf_import_resource_types.txt >"${accountfile}_account_blocks.tfstate"
+done
 
-# If using homebrew on MacOS, you need to install gnu-sed for this command to work. Default sed on MacOS is not the same as on Linux.
-if [[ -f /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed ]]; then
-    /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_config.tf
-    /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_zone_config.tf
-    /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ACCOUNT_ID/$CLOUDFLARE_ACCOUNT_ID/g" old_account_config.tf
-    /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_OLD_ACCOUNT_ID/$CLOUDFLARE_ACCOUNT_ID/g" old_account_zone_config.tf
-else
-    sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_config.tf
-    sed -i "s/$CLOUDFLARE_OLD_ZONE_ID/$CLOUDFLARE_ZONE_ID/g" old_account_zone_config.tf
-    sed -i "s/$CLOUDFLARE_OLD_ACCOUNT_ID/$CLOUDFLARE_ACCOUNT_ID/g" old_account_config.tf
-    sed -i "s/$CLOUDFLARE_OLD_ACCOUNT_ID/$CLOUDFLARE_ACCOUNT_ID/g" old_account_zone_config.tf
-fi
+counter=0
+for zonefile in $(ls -1 *_zone_config.tf); do
+    if [[ -s $zonefile ]]; then
+        CLOUDFLARE_ZONE_ID=$(echo "$zonefile" | cut -d'_' -f1)
+        CLOUDFLARE_NEW_ZONE_ID=${CLOUDFLARE_NEW_ZONE_IDS[$counter]}
+        echo "Attempting import of zone $CLOUDFLARE_ZONE_ID..."
+        echo "Transforming $CLOUDFLARE_ZONE_ID to $CLOUDFLARE_NEW_ZONE_ID..."
+        if [[ -f /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed ]]; then
+            /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "s/$CLOUDFLARE_ZONE_ID/$CLOUDFLARE_NEW_ZONE_ID/g" "$zonefile"
+        else
+            sed -i "s/$CLOUDFLARE_ZONE_ID/$CLOUDFLARE_NEW_ZONE_ID/g" "$zonefile"
+        fi
+    else
+        echo "WARN: Zone file $zonefile is empty or does not exist."
+    fi
+    echo .
+    echo .
+    echo "Beginning import of resources for zone $CLOUDFLARE_NEW_ZONE_ID..."
+    while read resource || [[ -n "$resource" ]]; do cf-terraforming import --resource-type "$resource" --modern-import-block --email "$CLOUDFLARE_EMAIL" --key "$CLOUDFLARE_API_KEY" --zone "$CLOUDFLARE_NEW_ZONE_ID" --token "$CLOUDFLARE_API_TOKEN"; done < cf_import_resource_types.txt >"${zonefile}_zone_blocks.tfstate"
+    counter=$((counter + 1))
+done
 
-# Terraform 1.5+ only
-while read resource || [[ -n $resource ]]; do cf-terraforming import --resource-type $resource --modern-import-block --email $CLOUDFLARE_EMAIL --key $CLOUDFLARE_API_KEY --zone $CLOUDFLARE_ZONE_ID; done < cf_import_resource_types.txt >cloudflare_import_blocks.tfstate
-
-terraform plan -generate-config-out=generated.tf
-
-terraform apply
+echo "Now you need to correct any errors during the cf-terraforming imports, make any necessary adjustments to the generated configs, then run terraform plan and terraform apply to see what happens."
